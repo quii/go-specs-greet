97d6dd53e9772ea6369c7d9919f53d1a0e65fa01
Chris James <quii@hey.com>
2022-09-30 09:39:44 +0100

    committing files helps

---
+++ specifications/curse.go
@@ -0,0 +1,17 @@
+package specifications
+
+import (
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+)
+
+type MeanGreeter interface {
+	Curse(name string) (string, error)
+}
+
+func CurseSpecification(t *testing.T, meany MeanGreeter) {
+	got, err := meany.Curse("Chris")
+	assert.NoError(t, err)
+	assert.Equal(t, got, "Go to hell, Chris!")
+}
